ee702ba38306cf78ef2b8d837d90cab261c640ec
unknown <D:\Mail\pwd>
2019-02-06 18:51:00 +0300

    v01

--- application.py
+++ application.py
@@ -168,7 +168,7 @@ def get_news_full():
     return "Для учащихся VIII - XI классов 20 декабря в 14.20 в актовом зале гимназии пройдёт единый день информирования \"Будущее Беларуси - это мы\" в рамках программы \"ШКОЛА АКТИВНОГО ГРАЖДАНИНА\""
 
 def get_req_sence(tokens):
-    reqSence = [0, 1]
+    reqSence = [0, 0, 0]
     classN = 9
     classL = 2
     weekDay = 1
@@ -271,79 +271,79 @@ def get_req_sence(tokens):
             reqSence[1] = reqSence[1] + 2
             continue
          
-#        if s.lower() in [
-#            '3',
-#            '3-ый'
-#            '3-ого'
-#            '3-ом'
-#            '3-ому'
-#            '3ый'
-#            '3ого'
-#            '3ом'
-#            '3ому'
-#            'три'
-#            'третий'
-#            'третьего'
-#            'третьем'
-#            'третьему'
-#        ]:
-#            reqSence[2] = reqSence[2] + 3
-#            if i < len(tokens) and tokens[i + 1].lower() in [
-#                'урок',
-#                'урока',
-#                'уроку',
-#                'уроке', 
-#            ] or i > 0 and tokens[i - 1].lower() in [
-#                'урок',
-#                'урока',
-#                'уроку',
-#                'уроке',  
-#            ] :
-#                lessonN = 3
-#            else :
-#                classN = 3
-#            continue         
-#         
-#        if s.lower() in [
-#            '9',
-#            '9-ый'
-#            '9-ого'
-#            '9-ом'
-#            '9-ому'
-#            '9ый'
-#            '9ого'
-#            '9ом'
-#            '9ому'
-#            'девять'
-#            'девятый'
-#            'девятого'
-#            'девятом'
-#            'девятому'
-#        ]:
-#            reqSence[2] = reqSence[2] + 3
-#            if i < len(tokens) and tokens[i + 1].lower() in [
-#                'урок',
-#                'урока',
-#                'уроку',
-#                'уроке', 
-#            ] or i > 0 and tokens[i - 1].lower() in [
-#                'урок',
-#                'урока',
-#                'уроку',
-#                'уроке',  
-#            ] :
-#                lessonN = 9
-#            else :
-#                classN = 9
-#            continue
+        if s.lower() in [
+            '3',
+            '3-ый'
+            '3-ого'
+            '3-ом'
+            '3-ому'
+            '3ый'
+            '3ого'
+            '3ом'
+            '3ому'
+            'три'
+            'третий'
+            'третьего'
+            'третьем'
+            'третьему'
+        ]:
+            reqSence[2] = reqSence[2] + 3
+            if i < len(tokens) and tokens[i + 1].lower() in [
+                'урок',
+                'урока',
+                'уроку',
+                'уроке', 
+            ] or i > 0 and tokens[i - 1].lower() in [
+                'урок',
+                'урока',
+                'уроку',
+                'уроке',  
+            ] :
+                lessonN = 3
+            else :
+                classN = 3
+            continue         
+         
+        if s.lower() in [
+            '9',
+            '9-ый'
+            '9-ого'
+            '9-ом'
+            '9-ому'
+            '9ый'
+            '9ого'
+            '9ом'
+            '9ому'
+            'девять'
+            'девятый'
+            'девятого'
+            'девятом'
+            'девятому'
+        ]:
+            reqSence[2] = reqSence[2] + 3
+            if i < len(tokens) and tokens[i + 1].lower() in [
+                'урок',
+                'урока',
+                'уроку',
+                'уроке', 
+            ] or i > 0 and tokens[i - 1].lower() in [
+                'урок',
+                'урока',
+                'уроку',
+                'уроке',  
+            ] :
+                lessonN = 9
+            else :
+                classN = 9
+            continue
         
-#    return reqSence.index(max(reqSence))
+    return reqSence.index(max(reqSence))
     if max(reqSence) < 4 :
         return answ[2]
     else :
         ind = reqSence.index(max(reqSence))
         if ind == 2 :
-#            token = classN * 100 + classL * 10 + weekDay
+            token = classN * 100 + classL * 10 + weekDay
             return 'Англ. яз'#timetable.getLessonName(token, lessonN)
         else :
             return info[reqSence.index(max(reqSence))]
